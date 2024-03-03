9cd5e05f1b0b27eb9428200aa79458c39ba2e7ad
dklaric00 <dario.klaric@fsre.sum.ba>
2024-03-03 19:58:47 +0100

    test

--- server.js
+++ server.js
@@ -2,3 +2,5 @@ const moment = require("moment");
 
 // Ispisuje koliko je vremena proslo od zadanog datuma
 console.log(moment([1999, 3, 20]).fromNow()); // 42 years ago
+
+console.log("Promjena!!!");
